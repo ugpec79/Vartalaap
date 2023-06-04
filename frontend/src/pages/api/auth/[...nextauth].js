9b80886b30be8bbd57ae0cdf0325cf23aba88fb3
Ujjawal Gupta <113056503+ugpec79@users.noreply.github.com>
2023-06-04 11:54:16 +0530

    Update [...nextauth].js

--- frontend/src/pages/api/auth/[...nextauth].js
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: "877971644707-q1mn0tclnrb6o5tdmoqett8vveiks9f8.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: "",
+      clientSecret: "",
     }),
     // ...add more providers here
   ],
